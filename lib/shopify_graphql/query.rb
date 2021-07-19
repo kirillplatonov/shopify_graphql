81a0afb842137775b8c89758bcfabc4f97900721
Kirill Platonov <mail@kirillplatonov.com>
2021-07-19 13:15:13 +0300

    Add query and mutation concerns

---
+++ lib/shopify_graphql/query.rb
@@ -0,0 +1,15 @@
+module ShopifyGraphql::Query
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def call(**kwargs)
+      new.call(**kwargs)
+    end
+  end
+
+  delegate :execute, :handle_user_errors, to: :client
+
+  def client
+    @client ||= ShopifyGraphql::Client.new
+  end
+end
