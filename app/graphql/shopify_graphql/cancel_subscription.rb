49bf852d2dd8f56799e1943fdb3bffc66ad0826e
Kirill Platonov <kirill@platonov.net>
2022-02-11 10:41:49 +0300

    Add subscription cancellation mutation

---
+++ app/graphql/shopify_graphql/cancel_subscription.rb
@@ -0,0 +1,34 @@
+module ShopifyGraphql
+  class CancelSubscription
+    include Mutation
+
+    MUTATION = <<~GRAPHQL
+      mutation appSubscriptionCancel($id: ID!) {
+        appSubscriptionCancel(id: $id) {
+          appSubscription {
+            id
+          }
+          userErrors {
+            field
+            message
+          }
+        }
+      }
+    GRAPHQL
+
+    def call(id:)
+      response = execute(MUTATION, id: id)
+      response.data = response.data.appSubscriptionCancel
+      handle_user_errors(response.data)
+      response.data = parse_data(response.data)
+      response
+    end
+
+    private
+
+    def parse_data(data)
+      subscription = OpenStruct.new(id: data.appSubscription.id)
+      OpenStruct.new(subscription: subscription)
+    end
+  end
+end
