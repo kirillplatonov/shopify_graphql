719fd80d8883bcd43bec81c443d75e1e73d55204
Kirill Platonov <kirill@platonov.net>
2024-07-22 09:37:12 +0200

    Remove support for Ruby 2.7 and Rails 6.0. Minimal required versions are Ruby 3.0 and Rails 6.1 (#42)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,14 +10,6 @@ ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __d
 require "rails/test_help"
 require "webmock/minitest"
 
-# Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
-  ActiveSupport::TestCase.fixtures :all
-end
-
 class ActiveSupport::TestCase
   API_PATH = "https://test-shop.myshopify.com/admin/api/2023-07/graphql.json"
 
