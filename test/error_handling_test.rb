551c5c4a88161678f3866452888758cab564e1cd
Kirill Platonov <kirill@platonov.net>
2024-10-21 08:48:08 +0200

    Move text error codes to #error_code field

--- test/error_handling_test.rb
+++ test/error_handling_test.rb
@@ -42,7 +42,22 @@ class ErrorHandlingTest < ActiveSupport::TestCase
       response = ShopifyGraphql.execute(SIMPLE_QUERY)
       ShopifyGraphql::Client.new.handle_user_errors(response.data.metafieldDefinitionCreate)
     rescue ShopifyGraphql::UserError => error
-      assert_equal "TAKEN", error.code
+      assert_equal 200, error.code
+      assert_equal "TAKEN", error.error_code
+      assert_includes error.message, "Key is in use for Product metafields"
+      assert_equal ["definition", "key"], error.fields
+    end
+  end
+
+  test "server error" do
+    fake("mutations/server_error.json", SIMPLE_QUERY)
+
+    begin
+      ShopifyGraphql.execute(SIMPLE_QUERY)
+    rescue ShopifyGraphql::ServerError => error
+      assert_equal 200, error.code
+      assert_equal "INTERNAL_SERVER_ERROR", error.error_code
+      assert_includes error.message, "Looks like something went wrong on our end"
     end
   end
 end
